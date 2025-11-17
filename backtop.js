d54681a3b166ef2bf98c1b9d68d229842f8188ee
Vlad Savu <vladsavu04@yahooo.com>
2025-11-17 13:21:30 +0200

    Proiect8

--- backtop.js
+++ backtop.js
@@ -1,5 +1,13 @@
 const backToTopButton = document.getElementById('backToTop');
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
 backToTopButton.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
